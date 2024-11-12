9517037a17d12226465a16c4c0fd3d6827181d9a
Zarrar Nawaz <mailzarrar30@yahoo.com>
2024-11-12 18:44:19 -0500

    Implement user register and login with authentication

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,68 @@
-#pip install fastapi uvicorn
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from passlib.context import CryptContext
+from jose import JWTError,jwt
+from datetime import datetime, timedelta, timezone
+import bcrypt
 
-# Create a FastAPI app
-# Root endpoint returns the app description
 app = FastAPI()
 
-#Stores the user's email, username, and password
-class User(BaseModel):
+SECRET_KEY = "bdd31c34fb1bb2bb93979bd30e7d628a8b18506aca574bad0266b2c0c608b57b"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+userDB = {}
+
+class Register(BaseModel):
     email: str
+    password: str
     username: str
+
+class Login(BaseModel):
+    email: str
     password: str
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-#In memory storage
-User_db: List[User] = []
 
-@app.get("/")
-async def read_root():
-    return("Welcome")
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
-#CRUD operations
-#POST
-@app.post("/user/", response_model=User)
-async def create_user ( new_user: User ):
-    User_db.append(new_user)
-    return new_user
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
-#GET
-@app.get("/user/{email}", response_model= List[User])
-async def get_user (email: str):
-    for user in User_db:
-        if user.email == email:
-            return user
-    raise HTTPException(status_code=404, detail="User not found")
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire =datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+@app.post("/api/register", status_code=status.HTTP_201_CREATED)
+async def register_user(user: Register):
+    if user.email in userDB:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    hashed_password = get_password_hash(user.password)
+    userDB[user.email] = {
+        "email": user.email,
+        "username": user.username,
+        "password": hashed_password
+            }
+    
+    return {"message": "User registered"}
+
+@app.post("/api/login")
+async def login_user(user: Login):
+    db_user = userDB.get(user.email)
+    if not db_user or not verify_password(user.password, db_user["password"]):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
+    
+    return {"token": access_token}
