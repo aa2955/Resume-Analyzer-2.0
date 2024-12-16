49029f7bb6d8d0a18173485cddc3c86dea3f4f30
Aileen Ni <aileenni2003@gmail.com>
2024-12-16 18:35:44 -0500

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -79,6 +79,7 @@ function App() {
   const handleLogout = () => {
     setIsLoggedIn(false);
     localStorage.removeItem("access_token");
+    return <Navigate to="/" />
   };
 
   const toggleDarkMode = () => {
